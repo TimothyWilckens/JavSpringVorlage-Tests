1b3daa53b61317f0676ca69240a661700007fcc8
TimothyWilckens <timothy.wilckens@gymhum.de>
2023-02-02 13:25:13 +0100

    funktioniert

--- JavaSpringVorlage-main/src/main/java/gymhum/de/SpielController.java
+++ JavaSpringVorlage-main/src/main/java/gymhum/de/SpielController.java
@@ -16,6 +16,7 @@ public class SpielController {
     public SpielController(){
         setFelder(new Feld[6][7]);
         initFeld();
+        TestLauf();
         showTestFeld();
     }
 
@@ -37,7 +38,43 @@ public class SpielController {
     private void showTestFeld(){
         for(int i = 0; i < 6; i++){
             for(int k = 0; k < 7; k++){
-                System.out.println(getFelder()[i][k].getIstFrei());
+                System.out.print(getFelder()[i][k].getIstFrei() + " ");
+            }
+        }
+    }
+
+    private void TestLauf(){
+        felder[2][2].setIstFrei(false);
+        felder[2][2].setZustand(false);
+        felder[3][2].setIstFrei(false);
+        felder[3][2].setZustand(false);
+        felder[4][2].setIstFrei(false);
+        felder[4][2].setZustand(false);
+        felder[5][2].setIstFrei(false);
+        felder[5][2].setZustand(false);
+        
+        for(int i = 0; i < 6; i++){
+            for(int k = 0; k < 4; k++){
+                if(getFelder()[i][k].getIstFrei() == false && getFelder()[i][k+1].getIstFrei() == false && getFelder()[i][k +2 ].getIstFrei() == false && getFelder()[i][k + 3].getIstFrei() == false){
+                    if(getFelder()[i][k].getZustand() && getFelder()[i][k+1].getZustand() && getFelder()[i][k +2 ].getZustand() && getFelder()[i][k + 3].getZustand()){
+                        System.out.println("Spieler O hat gewonnen");
+                    }
+                    else if(getFelder()[i][k].getZustand() == false && getFelder()[i][k+1].getZustand() == false && getFelder()[i][k +2 ].getZustand() == false && getFelder()[i][k + 3].getZustand() == false){
+                        System.out.println("Spieler X hat gewonnen");
+                    }
+                }
+            }
+        }
+        for(int hoehe = 0; hoehe < 3; hoehe++){
+            for(int breite = 0; breite < 7; breite++){
+                if(getFelder()[hoehe][breite].getIstFrei() == false && getFelder()[hoehe+1][breite].getIstFrei() == false && getFelder()[hoehe+2][breite].getIstFrei() == false && getFelder()[hoehe+3][breite].getIstFrei() == false){
+                    if(getFelder()[hoehe][breite].getZustand() && getFelder()[hoehe+1][breite].getZustand() && getFelder()[hoehe+2][breite].getZustand() && getFelder()[hoehe+3][breite].getZustand()){
+                        System.out.println("Spieler O hat gewonnen");
+                    }
+                    else if(getFelder()[hoehe][breite].getZustand() == false && getFelder()[hoehe+1][breite].getZustand() == false && getFelder()[hoehe+2][breite].getZustand() == false && getFelder()[hoehe+3][breite].getZustand() == false){
+                        System.out.println("Spieler X hat gewonnen");
+                    }
+                }
             }
         }
     }
@@ -48,5 +85,6 @@ public class SpielController {
     public Feld[][] getFelder() {
         return felder;
     }
+
 
 }
